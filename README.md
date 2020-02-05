650e5b47e7b89673fc13dccd32c920164d0ded06
bijay135 <bijayshah975@gmail.com>
2020-02-05 16:31:12 +0545

    initialized project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Files Filter Using Excel For Windows
+- This program will filter and move files based on excel entries to a diffrent folder
+- It can be useful to organize files in a meta folder
+- The correct file references needs to be in a excel spreadsheet
+
+# Technologies Used
+- Powershell Scripting
+- Command Prompt
+
+# How to use
+- Extract the zip contents, make sure all the files are in same folder
+- Copy the files from meta folder into 'MetaFolder\'
+- Open the run-program.bat file
+- Enter the full path to the excel file example `F:\files-filter-excel\excel_file.xslx`
+- Enter the specefic sheet name example `sheet1`
+- Enter the new folder to move filtered items to example `F:\files-filter-excel\FilteredFolder`
+- Program will match the excel entries with filesystem names
+- If the names match it will move each files to the specified new folder
+- When all files have been moved it will display success message
---
+++ filter.ps1
@@ -0,0 +1,32 @@
+﻿$excelFile = Read-Host -Prompt "Input your full excel file path"
+$excelSheet = Read-Host -Prompt "Input your excel sheet name"
+$metaFolder = ".\MetaFolder"
+$filteredFolder = Read-Host -Prompt "Input new folder to move filtered images to"
+
+$excel = New-Object -comobject Excel.Application
+$workbook = $excel.Workbooks.Open($excelFile)
+$worksheet = $workbook.Worksheets.Item($excelSheet)
+$workSheet.Name
+
+$imageNames = @()
+$i = 2
+while ($worksheet.Cells.Item($i, 2).Value() -ne $null) {
+    $imageNames += $worksheet.Cells.Item($i, 2).Value()
+    $i++
+}
+   
+$workbook.Close()
+$excel.Quit()
+# IMPORTANT: clean-up used Com objects
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
+[System.GC]::Collect()
+[System.GC]::WaitForPendingFinalizers()
+
+Get-ChildItem -Path $metaFolder |
+  Where-Object { $imageNames -contains $_.Name } |
+  Move-Item -Destination $filteredFolder -verbose
+
+Write-Output "All files successfully moved"
+Start-Sleep -s 3
